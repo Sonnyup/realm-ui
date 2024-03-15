7d0398af1ff4f1c26d5d37382d86697c1c392ca8
洛羽 <344548747@qq.com>
2024-03-15 17:34:04 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <script setup lang="ts">
-import { h } from 'vue'
-import { NButton, NDataTable, NTag } from 'naive-ui';
+import { h, ref } from 'vue'
+import { NButton, NDataTable, NTag, NGradientText, NModal, NForm, NInput, NFormItem } from 'naive-ui';
 
 const columns = [
   {
-    title: '本地IP',
+    title: '本地地址',
     key: 'local_host',
     width: 200,
     // resizable: true,
+    ellipsis: true,
     render(row) {
       return h(
-        NTag,
+        NGradientText,
         {
           style: {
-            marginRight: '6px'
+            marginRight: '6px',
+            fontSize: '13px',
           },
           type: 'success',
-          bordered: false,
-          size: 'small'
         },
         {
           default: () => row.local_host
@@ -49,23 +49,23 @@ const columns = [
     }
   },
   {
-    title: '远程IP',
+    title: '远程地址',
     key: 'remote_host',
     // resizable: true,
     width: 200,
+    ellipsis: true,
     render(row) {
       return h(
-        NTag,
+        NGradientText,
         {
           style: {
-            marginRight: '6px'
+            marginRight: '6px',
+            fontSize: '13px',
           },
           type: 'info',
-          bordered: false,
-          size: 'small'
         },
         {
-          default: () => row.remote_host
+          default: () => row.local_host
         }
       )
     }
@@ -81,7 +81,7 @@ const columns = [
         NTag,
         {
           style: {
-            marginRight: '6px'
+            marginRight: '6px',
           },
           type: 'info',
           bordered: false,
@@ -96,18 +96,46 @@ const columns = [
   {
     title: '操作',
     key: 'action',
-    width: 50,
+    width: 100,
+    align: 'center',
     render(row) {
-      return h(
-        NButton,
-        {
-          strong: true,
-          tertiary: true,
-          size: "small",
-          onClick: () => play(row)
-        },
-        { default: () => "删除" }
-      );
+      return [
+        h(
+          NButton,
+          {
+            strong: true,
+            // tertiary: true,
+            type: "primary",
+            size: "small",
+            onClick: () => play(row)
+          },
+          { default: () => "开启" }
+        ),
+        // h(
+        //   NButton,
+        //   {
+        //     strong: true,
+        //     // tertiary: true,
+        //     type: "warning",
+        //     size: "small",
+        //     onClick: () => play(row)
+        //   },
+        //   { default: () => "关闭" }
+        // ),
+        h(
+          NButton,
+          {
+            strong: true,
+            type: "error",
+            size: "small",
+            style: {
+              marginLeft: '8px'
+            },
+            onClick: () => play(row)
+          },
+          { default: () => "删除" }
+        )
+      ]
     }
   }
 ];
@@ -139,20 +167,86 @@ const data = [
 const pagination = {
   pageSize: 2,
 };
+
+const bodyStyle = {
+  width: '400px'
+};
+const segmented = {
+  content: 'soft',
+  footer: 'soft'
+};
+
+const addConfig = ref({
+  local_host: '0.0.0.0',
+  local_port: '80800',
+  remote_host: '2132:0568:0123:1223:0DA8:0D45:0000:52D3',
+  remote_port: '8080',
+  protocol: ['udp', 'tcp']
+});
+
+const showModal = ref(false);
 </script>
 
 <template>
   <div class="container">
-    <n-data-table :size="small" :min-height="540" :columns="columns" :data="data" :pagination="pagination" :bordered="false" :single-line="false"/>
+    <n-data-table style="height:100%;" size="small" :columns="columns" :data="data" :pagination="pagination"
+      :bordered="false" :single-line="false" />
+    <n-button class="add-btn" type="primary" size="small" @click="showModal = true">添加</n-button>
   </div>
+
+  <!-- 添加转发 -->
+  <n-modal v-model:show="showModal" class="custom-card" preset="card" :style="bodyStyle" title="添加转发" size="small"
+    :bordered="false" :segmented="segmented">
+    <template #header-extra>
+    </template>
+    <n-form ref="formRef" :model="addConfig">
+      <n-form-item path="local_host" label="本地地址（IP、域名）">
+        <n-input v-model:value="addConfig.local_host" @keydown.enter.prevent />
+      </n-form-item>
+      <n-form-item path="local_port" label="本地端口">
+        <n-input v-model:value="addConfig.local_port" @keydown.enter.prevent />
+      </n-form-item>
+      <n-form-item path="remote_host" label="远程地址（IP、域名）">
+        <n-input v-model:value="addConfig.remote_host" @keydown.enter.prevent />
+      </n-form-item>
+      <n-form-item path="remote_port" label="远程端口">
+        <n-input v-model:value="addConfig.remote_port" @keydown.enter.prevent />
+      </n-form-item>
+      <n-form-item label="转发协议" path="protocol">
+        <n-checkbox-group v-model:value="model.protocol">
+          <n-space>
+            <n-checkbox value="tcp">
+              TCP
+            </n-checkbox>
+            <n-checkbox value="udp">
+              UDP
+            </n-checkbox>>
+          </n-space>
+        </n-checkbox-group>
+      </n-form-item>
+    </n-form>
+    <template #footer>
+      <n-button class="add-btn" type="primary" size="small">确认</n-button>
+    </template>
+  </n-modal>
 </template>
 
-<style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
+<style lang="scss" scoped>
+.container {
+  height: 100%;
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
+  :deep(.n-data-table .n-data-table__pagination) {
+    margin-top: 0;
+    margin-right: 15px;
+    height: 35px;
+  }
+
+  .add-btn {
+    position: fixed;
+    left: 15px;
+    bottom: 10px;
+  }
 }
 </style>
+
+<style></style>
